5e6451037884b2d312212fc32d164aabbc1f301b
guilhermepardo <guilherme.pardoanjos@gmail.com>
2021-05-01 06:31:07 -0300

    created nav and menu

--- btshirt-frontend/src/app/app.module.ts
+++ btshirt-frontend/src/app/app.module.ts
@@ -3,24 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http'
  
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/template/navbar/navbar.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { MenuComponent } from './components/template/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
